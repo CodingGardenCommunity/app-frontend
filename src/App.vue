da9b692bf25dbe9ae8105d729140e91ba65e8ce2
Jaman Brundage <jbrundage372@gmail.com>
2019-08-03 14:27:23 -0400

    Added more partially finished vue frontend components
    Signed-off-by: Jaman Brundage <jbrundage372@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="topnav">
-      <div id="menu-icon" v-on:click="toggleMenu">
+      <div id="menu-icon" @click="toggleMenu">
         |||
       </div>
       <div id="filter-by">
@@ -13,10 +13,7 @@
       </div>
     </div>
     <div class="app-container">
-      <div
-        :class="sidebarOpen ? 'open' : ''"
-        class="side-bar"
-      >
+      <div :class="sidebarOpen ? 'open' : ''" class="side-bar">
         <ul class="side-bar-menu"></ul>
       </div>
       <router-view class="router-view"/>
@@ -50,116 +47,26 @@ export default {
   data() {
     return {
       sidebarOpen: false,
-    }
+      menuData: [
+        { label: 'videos', asdf: 'videos-icon.svg' },
+        { label: 'history', asdf: 'history-icon.svg' },
+        { label: 'FAQ', asdf: 'faq-icon.svg' },
+        { label: 'contributors', asdf: 'contributors-icon.svg' },
+      ],
+    };
   },
   components: {
     Leaves,
   },
   methods: {
-    toggleMenu: function(){
+    toggleMenu() {
       this.sidebarOpen = !this.sidebarOpen;
-    }
-  }
+    },
+  },
 };
 </script>
 
 
 <style>
-body{
-  margin: 0;
-  padding: 0;
-}
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  display: flex;
-  flex-direction: column;
-  width: 100vw;
-  height:100vh;
-}
-#app > .page{
-  flex: 1;
-}
-#topnav{
-    display: flex;
-    min-height: 80px;
-    background-color: #2c3e50;
-    flex: 0;
-}
-#topnav > #menu-icon{
-    flex: 0;
-    text-align: center;
-    min-width: 80px;
-    line-height: 80px;
-    transform: rotate(90deg);
-    color: white;
-    font-size: 40px;
-}
-#topnav > #filter-by{
-    color: white;
-    flex: 1;
-    text-align: left;
-    display: flex;
-    flex-direction: column;
-}
-#topnav > #filter-by > div{
-    color: white;
-    flex: 1;
-    text-align: left;
-}
-#topnav > #filter-by > div.label{
-    line-height: 40px;
-}
-#topnav > #search{
-    flex: 1;
-    text-align: center;
-    min-width: 80px;
-    padding-top: 20px;
-    width: 100%;
-}
-#topnav > #search > input{
-    background-color: transparent;
-    border-width: 0 0 2px 0;
-    line-height: 40px;
-}
-#topnav > #search > input::placeholder{
-    color: white;
-}
-#bottomnav{
-    display: flex;
-    min-height: 80px;
-    background-color: #2c3e50;
-    flex: 0;
-    color: white;
-}
-#bottomnav > a{
-    flex: 1;
-    text-align: center;
-    font-size: 10px;
-    color: white;
-    text-decoration: none;
-    padding-top: 25px;
-}
-.app-container {
-  display: flex;
-  height: 100%;
-}
-.side-bar {
-  min-width: 0;
-}
-.side-bar.open {
-  min-width: 20rem;
-}
-.side-bar-menu{
-  padding:0;
-  margin:0;
-  background-color:red;
-}
-.router-view {
-  flex: 4;
-  background: #000;
-}
+@import url('./assets/css/main.css');
 </style>
--- src/views/Videos.vue
+++ src/views/Videos.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="view-container">
-    Videos page
+    <div class="view-header">
+      <h1>Videos</h1>
+    </div>
+    <ul class="data-list">
+      <li v-for="(item, index) in dataItems" :key="index" class="data-item">
+
+        <div class="video-item">
+          {{ item.name }}
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -8,5 +18,32 @@
 export default {
   name: 'videos',
   components: {},
+  data() {
+    return {
+      value: '',
+      dataItems: [
+        {
+          name: 'asdf123',
+          thumbnail: 'asdf.png',
+          title: 'some title',
+          description: 'some description',
+          url: 'asdf.com',
+          leaves: '',
+        },
+      ],
+    };
+  },
+  created() {
+    fetch('https://cors-adder.joshuaferr1s.now.sh/cors/https:/api-dev.coding.garden/video')
+      .then(n => n.json())
+      .then((json) => {
+        console.log(json);
+        this.dataItems = json.map(n => n.attributes);
+      });
+  },
 };
 </script>
+
+<style scoped>
+
+</style>
