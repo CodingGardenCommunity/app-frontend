3f0aa4c0a507e1bb67c8d8fb87f9a9b393fdd4fc
Jaman Brundage <jbrundage372@gmail.com>
2019-08-07 01:22:01 -0400

    Removed views/Menu.vue test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -4,7 +4,6 @@ import Contributors from '@/views/Contributors.vue';
 import FAQ from '@/views/FAQ.vue';
 import History from '@/views/History.vue';
 import Home from '@/views/Home.vue';
-import Menu from '@/views/Menu.vue';
 import Videos from '@/views/Videos.vue';
 
 describe('About.vue', () => {
@@ -51,16 +50,6 @@ describe('Home.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'Home';
     const wrapper = shallowMount(Home, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
-});
-
-describe('Menu.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'Menu';
-    const wrapper = shallowMount(Menu, {
       propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
