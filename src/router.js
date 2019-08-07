8164aa92c1656ce2af8809dbc931602172b55622
Jaman Brundage <jbrundage372@gmail.com>
2019-08-06 23:41:56 -0400

    Removed some codemirror references

--- src/router.js
+++ src/router.js
@@ -1,12 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import VueCodemirror from 'vue-codemirror';
 import History from './views/History.vue';
 import FAQ from './views/FAQ.vue';
 import Videos from './views/Videos.vue';
 import Contributors from './views/Contributors.vue';
 
-Vue.use(VueCodemirror);
 Vue.use(Router);
 
 export default new Router({
