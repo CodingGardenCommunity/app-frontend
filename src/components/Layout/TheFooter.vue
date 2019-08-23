a9fe84cff3e50bf29a22ac691119f55b47e5b6bb
Tejas H <tejas007tez@gmail.com>
2019-08-23 15:06:15 +0530

    Added TheFooter component.

---
+++ src/components/Layout/TheFooter.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="footer">
+    <span> {{ `&copy; ${year}` }} | Built with love by <a target="_blank" href="https://github.com/orgs/CodingGardenCommunity/people">CodingGarden Community</a> </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter',
+  data() {
+    return {
+      year: new Date().getFullYear(),
+    };
+  },
+};
+</script>
+
+<style scoped>
+#footer {
+  display: flex;
+  min-height: 50px;
+  color: black;
+  background-color: white;
+  box-shadow: 0 0 10px -8px black;
+}
+span {
+  flex: 1;
+  margin: auto;
+  text-align: center;
+  font-size: 12px;
+  color: gray;
+  text-decoration: none;
+}
+
+a {
+  color: #0066ffc0;
+  text-decoration: none;
+}
+</style>
