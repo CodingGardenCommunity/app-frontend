83768c937f6f6246f3c387d5b833fb425906ac0b
Tejas H <tejas007tez@gmail.com>
2019-08-27 17:34:17 +0530

    Removed HelloWorld.vue file & updated sample test.

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,5 @@
-import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
-
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+describe('This is a sample test', () => {
+  it('Should be zero', () => {
+    expect(0).toBe(0);
   });
 });
